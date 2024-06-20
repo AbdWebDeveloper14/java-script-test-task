360822deb9895c323c8683ac48ae773edf3faab6
abdwebdeveloper14 <khan99hussain@gmail.com>
2024-06-20 17:21:15 +0500

    everything

--- assingment1.js
+++ assingment1.js
@@ -1,36 +1,36 @@
 
 let a = "10"
 console.log("a");
-document.write(a + "<br>")
+document.write( "The valuae of a is :"+ " " +a + "<br>")
 "<br>"
 let resulta = ++a
 console.log(resulta);
-document.write(resulta + "<br>")
+document.write( "The valuae of ++a is :"+" "+resulta + "<br>")
 "<br>"
 let b = resulta
 console.log(b);
-document.write(b + "<br>")
+document.write( "Now the valuae of a is :"+ " " +b + "<br>")
 "<br>"
 let resultb = +b
 console.log(resultb);
-document.write(resultb + "<br>")
+document.write(" The valuae of a++ is :" + " " +resultb + "<br>")
 "<br>"
 let c = +11+1
 console.log(c);
-document.write(c + "<br>")
+document.write("Now the valuae of a is :"+ " " + c + "<br>")
 "<br>"
 let resultc = --c
 console.log(resultc);
-document.write(resultc + "<br>")
+document.write(" the valuae of --a is :"+ " " + resultc + "<br>")
 "<br>"
 let d = resultc
 console.log(resultc);
-document.write(resultc + "<br>")
+document.write("Now the valuae of a is :"+ " " +resultc + "<br>")
 "<br>"
 let aresult = +10+1
 console.log(aresult);
-document.write(aresult + "<br>")
+document.write(" The valuae of a is :"+ " " +aresult + "<br>")
 "<br>"
 let bresult = aresult+1
 console.log(bresult);
-document.write(bresult + "<br>")
+document.write("Now the valuae of a is :"+ " " +bresult + "<br>")
